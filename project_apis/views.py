a93603b6b9db2e16ae4511f2643662200d11b85e
Heman <hemu6119@gmail.com>
2020-10-10 17:59:29 +0530

    Added filters and permissions

--- project_apis/views.py
+++ project_apis/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from project_apis import serializers
+from project_apis import models
+from rest_framework.authentication import TokenAuthentication
+from project_apis import permissions
+from rest_framework import filters
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -91,3 +95,13 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email', )
